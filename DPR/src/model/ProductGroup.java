654230b84e8ac3693c88ca055818d41eddbc426a
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-10 08:49:54 +0200

    Update ProductGroup.java

--- DPR/src/model/ProductGroup.java
+++ DPR/src/model/ProductGroup.java
@@ -1,5 +1,30 @@
 package model;
 
 public class ProductGroup {
+	private String groupName;
+	private int groupWarningPeriod;
+	
+	public ProductGroup(String groupName, int groupWarningPeriod) {
+		super();
+		this.groupName = groupName;
+		this.groupWarningPeriod = groupWarningPeriod;
+	}
 
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public int getGroupWarningPeriod() {
+		return groupWarningPeriod;
+	}
+
+	public void setGroupWarningPeriod(int groupWarningPeriod) {
+		this.groupWarningPeriod = groupWarningPeriod;
+	}
+	
+	
 }
