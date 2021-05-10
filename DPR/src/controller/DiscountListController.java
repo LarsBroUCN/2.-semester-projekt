dd14196c8dbf6596c24f294723188c9917039b28
LarsBroUCN <1086256@ucn.dk>
2021-05-10 10:39:06 +0200

    Metode cotroller kal

--- DPR/src/controller/DiscountListController.java
+++ DPR/src/controller/DiscountListController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import java.util.List;
+
+import model.Batch;
+
 public class DiscountListController {
 
+	public static List<Batch> generateDiscountList() {
+		// TODO Auto-generated method stub	
+		return null;
+	}
+	
 }
--- DPR/src/controller/PendingListController.java
+++ DPR/src/controller/PendingListController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import java.util.List;
+
+import model.Batch;
+
 public class PendingListController {
+
+	public static List<Batch> generatePendingList() {
+		// TODO Auto-generated method stub	
+		return null;
+	}
 
 }
