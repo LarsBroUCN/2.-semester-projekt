c89bbcc1d89094099a74895f6013fa98ffe26586
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-10 09:32:41 +0200

    Update Batch.java

--- DPR/src/model/Batch.java
+++ DPR/src/model/Batch.java
@@ -8,55 +8,66 @@ public class Batch {
 	private LocalDate arrivalDate;
 	private int warningPeriod;
 	private LocalDate expirationDate;
+	private Product product;
+	private Notification notification;
 	
-	
-	public Batch(int batchID, LocalDate arrivalDate, int warningPeriod, LocalDate expirationDate) {
+	public Batch(int batchID, LocalDate arrivalDate, int warningPeriod, LocalDate expirationDate, Product product,
+			Notification notification) {
+		super();
 		this.batchID = batchID;
 		this.arrivalDate = arrivalDate;
 		this.warningPeriod = warningPeriod;
 		this.expirationDate = expirationDate;
+		this.product = product;
+		this.notification = notification;
 	}
-
 
 	public int getBatchID() {
 		return batchID;
 	}
 
-
 	public void setBatchID(int batchID) {
 		this.batchID = batchID;
 	}
-
 
 	public LocalDate getArrivalDate() {
 		return arrivalDate;
 	}
 
-
 	public void setArrivalDate(LocalDate arrivalDate) {
 		this.arrivalDate = arrivalDate;
 	}
-
 
 	public int getWarningPeriod() {
 		return warningPeriod;
 	}
 
-
 	public void setWarningPeriod(int warningPeriod) {
 		this.warningPeriod = warningPeriod;
 	}
-
 
 	public LocalDate getExpirationDate() {
 		return expirationDate;
 	}
 
-
 	public void setExpirationDate(LocalDate expirationDate) {
 		this.expirationDate = expirationDate;
 	}
-	
-	
 
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Notification getNotification() {
+		return notification;
+	}
+
+	public void setNotification(Notification notification) {
+		this.notification = notification;
+	}
+		
 }
