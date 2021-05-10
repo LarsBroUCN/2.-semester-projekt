f1837152048d63b17188f54dca291c761228c269
perwolf <1086271@ucn.dk>
2021-05-10 09:28:24 +0200

    Batch i modellag oprettet

--- DPR/src/model/Batch.java
+++ DPR/src/model/Batch.java
@@ -1,5 +1,62 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Batch {
+	
+	private int batchID;
+	private LocalDate arrivalDate;
+	private int warningPeriod;
+	private LocalDate expirationDate;
+	
+	
+	public Batch(int batchID, LocalDate arrivalDate, int warningPeriod, LocalDate expirationDate) {
+		this.batchID = batchID;
+		this.arrivalDate = arrivalDate;
+		this.warningPeriod = warningPeriod;
+		this.expirationDate = expirationDate;
+	}
+
+
+	public int getBatchID() {
+		return batchID;
+	}
+
+
+	public void setBatchID(int batchID) {
+		this.batchID = batchID;
+	}
+
+
+	public LocalDate getArrivalDate() {
+		return arrivalDate;
+	}
+
+
+	public void setArrivalDate(LocalDate arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+
+	public int getWarningPeriod() {
+		return warningPeriod;
+	}
+
+
+	public void setWarningPeriod(int warningPeriod) {
+		this.warningPeriod = warningPeriod;
+	}
+
+
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+
+
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	
+	
 
 }
