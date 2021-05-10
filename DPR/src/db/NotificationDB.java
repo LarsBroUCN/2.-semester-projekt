5998b7f28d056a389fda39abf4cf326d47a6fae3
LeonNorsk <76209144+LeonNorsk@users.noreply.github.com>
2021-05-10 13:48:20 +0200

    no message

--- DPR/src/db/NotificationDB.java
+++ DPR/src/db/NotificationDB.java
@@ -1,15 +1,59 @@
 package db;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Notification;
 import model.Status;
 
 public class NotificationDB implements NotificationDBIF {
-
+	private static final String FINDSTATEQ = "select * from notifications where Status =?";
+	private PreparedStatement findState;
+	
+	public NotificationDB() throws DataAccessException {
+		try {
+			findState = DBConnection.getInstance().getConnection().prepareStatement(FINDSTATEQ);
+		} catch (SQLException e) {
+			throw new DataAccessException(e, "could not prepare statement");
+		}
+	}
+	
+	
+	//findall notifications
 	@Override
-	public Status getstate() throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Notification> getstate() throws DataAccessException {
+		try {
+			ResultSet rs = findState.executeQuery();
+			List<Notification> res = buildObjects(rs);
+			return res;			
+		} catch (Exception e) {
+			throw new DataAccessException(null, "Kunne ikke finde nogle informationer, om værksteder osv.");
+		}
 	}
 
+	private List<Notification> buildObjects(ResultSet rs) throws SQLException {
+		List<Notification> res = new ArrayList<>();
+		while(rs.next()) {
+			res.add(buildObject(rs));
+		}
+		return res;
+	}
+
+
+	private Notification buildObject(ResultSet rs) throws SQLException {
+		Notification n = new Notification(
+				rs.getString("note"),
+			 	rs.getDouble(""),
+			 	Status.valueOf(rs.getString("status")) 
+				);
+		return n;
+	}
+
+
+	//update
 	@Override
 	public void setStateExpired(int notificationID) throws DataAccessException {
 		// TODO Auto-generated method stub
--- DPR/src/db/NotificationDBIF.java
+++ DPR/src/db/NotificationDBIF.java
@@ -1,10 +1,13 @@
 package db;
 
+import java.util.List;
+
+import model.Notification;
 import model.Status;
 
 public interface NotificationDBIF {
 	
-	public Status getstate() throws DataAccessException;
+	public List<Notification> getstate() throws DataAccessException;
 	
 	public void setStateExpired(int notificationID) throws DataAccessException;
 
