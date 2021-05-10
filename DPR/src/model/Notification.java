30f657262c1224282126f8f4f84ba02e0fefd959
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-10 08:59:13 +0200

    Update Notification.java

--- DPR/src/model/Notification.java
+++ DPR/src/model/Notification.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Notification {
+	private String note;
+	private double discount;
+	private Status status;
+	
+	public Notification(String note, double discount, Status status) {
+		super();
+		this.note = note;
+		this.discount = discount;
+		this.status = status;
+	}
 
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	
+	
 }
