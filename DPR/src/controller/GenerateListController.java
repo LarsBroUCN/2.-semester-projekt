12fe89584bfc5c6a18aae8e7b3f2937e804da021
LarsBroUCN <1086256@ucn.dk>
2021-05-10 12:56:38 +0200

    GenerateListController

--- DPR/src/controller/GenerateListController.java
+++ DPR/src/controller/GenerateListController.java
@@ -1,17 +1,44 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Batch;
+import model.Notification;
 import model.Status;
-
 
 public class GenerateListController {
 
-	public List<Batch> generateList(Status status) {
-		PendingListController.generatePendingList();
-		return null;
+	private ArrayList<Batch> batchList = new ArrayList<Batch>();
+	private ArrayList<Notification> notificationList = new ArrayList<Notification>();
+	
+	public ArrayList<Notification> generateList(Status status) {
+		ArrayList<Batch> expiredList = (ArrayList<Batch>) ExpiredListController.generateExpiredList();
+		ArrayList<Batch> pendingList = (ArrayList<Batch>) PendingListController.generatePendingList();
+		ArrayList<Batch> discountList = (ArrayList<Batch>) DiscountListController.generateDiscountList();
+		
+		//join batchLists
+		addToBatchList(expiredList);
+		addToBatchList(pendingList);
+		addToBatchList(discountList);
+		
+		createNotificationListFromBatchList(batchList); //Find notifications from batchList and transfer to notificationList
+		
+		ArrayList<Notification> notificationListcopy = (ArrayList<Notification>) notificationList.clone(); // create copy of list notificationList
+		
+		return notificationListcopy; //return copy of list
 	}
 	
+	private void createNotificationListFromBatchList(ArrayList<Batch> batches){
+		for(Batch batch : batches) {
+			notificationList.add(batch.getNotification());
+		}
+	}
+	
+	private void addToBatchList(ArrayList<Batch> batches) {
+		for(Batch batch : batches) {
+			batchList.add(batch);
+		}
+	}
 	
 }
