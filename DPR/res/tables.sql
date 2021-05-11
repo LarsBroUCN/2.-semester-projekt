e9e56bccf7427f585bf1ee5eaa8614a9bb70cb56
LeonNorsk <76209144+LeonNorsk@users.noreply.github.com>
2021-05-11 08:57:34 +0200

    BatchDB ændret plau tilføjet ændringer scripts

--- DPR/res/tables.sql
+++ DPR/res/tables.sql
@@ -9,7 +9,71 @@ use dmab0920_1086231
 --drop table BatchSaleSpot
 --drop table Edges
 
- --create table Productgroups(
+
+--insert into Productgroups (groupname, groupWarningPeriod) values ('Kød', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('Kolonial', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('husholdning', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('frugt & grønt', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('Grovvarer', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('Tekstil & sko', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('Bolig & fritid', 12)
+--insert into Productgroups (groupname, groupWarningPeriod) values ('Brød, bageri & mejeri', 12)
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (122121, 'fisk', 12, 25, 'kød')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (11111, 'flåede tomater', 12, 25, 'Kolonial')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (22222, 'Helsekost', 12, 25, 'husholdning')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (33333, 'Helsekost', 12, 15, 'frugt & grønt')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (44444, 'korn,foderstoffer', 12, 50, 'Grovvarer')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (55555, 'Dame', 12, 50, 'Tekstil & sko')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (66666, 'Hus og Have', 12, 300, 'Bolig & fritid')
+--insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (77777, 'Hus og Have', 12, 10, 'Brød, bageri & mejeri')
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 122121)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 11111)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 22222)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 33333)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 44444)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 55555)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 66666)
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('10/05/2021','10/05/2021',  12, 77777)
+--insert into notifications (discount, note, status, batchID_fk) values (25, '', 'pending', 1 )
+--insert into notifications (discount, note, status, batchID_fk) values (10, '', 'pending', 4)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'pending', 5)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'pending', 6)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'discount', 7)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'complete', 8)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'discard', 9)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'expired', 10)
+
+ -- update notifications set discount = 30, note = '' , status = 'expired'  where batchid_fk = 5
+
+
+  select notifications.batchid_fk, notifications.status from notifications
+ inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'discount'
+
+   select notifications.batchid_fk, notifications.status from notifications
+ inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'pending'
+
+  select notifications.batchid_fk, notifications.status from notifications
+ inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'complete'
+
+  select notifications.batchid_fk, notifications.status from notifications
+ inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'discard'
+
+   select notifications.batchid_fk, notifications.status from notifications
+ inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'expired'
+
+ select * from Productgroups
+ select * from products
+ select * from batches
+ select * from notifications
+ select * from batchsalespot
+ select * from salespot
+ select * from nodes
+ select * from edges
+
+
+
+
+ -- create table Productgroups(
  --   groupname varchar(64) primary key,
  --   groupWarningPeriod int    
  --)
@@ -24,10 +88,6 @@ use dmab0920_1086231
  --    foreign key (groupname_fk) references Productgroups(groupname)
  --)
 
-
-
-
-
  --create table Batches(
 	--batchID int primary key identity(1,1),
 	--arrivaldate date not null,
@@ -38,11 +98,12 @@ use dmab0920_1086231
  --)
 
 
+
  --create table Notifications(
 	--notificationID int primary key identity(1,1),
 	--discount float,
 	--note varchar(300),
-	--status varchar(64) not null,
+	--status varchar(200) not null check(status IN ('Pending','Discount','Expired','Discard','Complete')),
 	--batchID_fk int not null,
 	--foreign key (batchID_fk) references Batches(batchID) 
  --)
@@ -72,15 +133,6 @@ use dmab0920_1086231
 	--nodename1_fk varchar(200) not null,
 	--nodename2_fk varchar(200) not null,
 	--foreign key (nodename1_fk) references Nodes(nodeName),
-	--foreign key (nodename2_fk) references Nodes(nodeName)
-	
+	--foreign key (nodename2_fk) references Nodes(nodeName)	
  --)
 
- select * from Productgroups
- select * from products
- select * from batches
- select * from notifications
- select * from batchsalespot
- select * from salespot
- select * from nodes
- select * from edges
--- DPR/src/db/BatchDB.java
+++ DPR/src/db/BatchDB.java
@@ -1,81 +1,65 @@
 package db;
 
 import java.sql.PreparedStatement;
-<<<<<<< HEAD
 import java.sql.ResultSet;
-=======
->>>>>>> c9775a4c3a24002cd5070abe4ae9e294ad27d30c
 import java.sql.SQLException;
-
 import model.Batch;
-import model.Product;
 import model.Status;
 
 public class BatchDB implements BatchDBIF {
-<<<<<<< HEAD
-	
-	private final String SEARCH_BATCH_Q = "select * from batch where BatchID = ?";
-	private PreparedStatement searchBatch;
-	
-	public BatchDB() throws DataAccessException {
-		try {
-			searchBatch = db.DBConnection.getInstance().prepareStatement(SEARCH_BATCH_Q);
-		} catch(SQLException e) {
-			throw new DataAccessException(e, "Could not prepare statement");
-		}
-	}
-	
-	public Batch searchBatch(int batchID) throws DataAccessException {
-		try {
-			searchBatch.setInt(1, batchID);
-			ResultSet rs = searchBatch.executeQuery();
-			Batch b = null;
-			if(rs.next()) {
-				b = buildObject(rs);
-			}
-			return b;
-		} catch (SQLException e) {
-			throw new DataAccessException(e, "Could not retrieve data");
-		}
-		}
-	private Batch buildObject(ResultSet rs) throws DataAccessException {
-		try {
-			Batch b = null;
-			if(rs.getString(columnIndex).equals)
-		}
-		return null;
-=======
-	private static final String  FINDSTATUSQ = "    select notifications.batchid_fk, notifications.status from notifications\r\n"
+	private static final String  FINDSTATUSQ = "select notifications.batchid_fk, notifications.status from notifications\r\n"
 			+ " inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = ?";
-	private PreparedStatement findstatus;
+	private final String SEARCH_BATCH_Q = "select * from batch where BatchID = ?";
+	private PreparedStatement findstatus, searchBatch;
 	
 	public BatchDB() throws SQLException {
 		findstatus = DBConnection.getInstance().getConnection().prepareStatement(FINDSTATUSQ);
->>>>>>> c9775a4c3a24002cd5070abe4ae9e294ad27d30c
+        searchBatch = DBConnection.getInstance().getConnection().prepareStatement(SEARCH_BATCH_Q);
 	}
 	
+	   public Batch searchBatch(int batchID) throws DataAccessException {
+	        try {
+	            searchBatch.setInt(1, batchID);
+	            ResultSet rs = searchBatch.executeQuery();
+	            Batch b = null;
+	            if(rs.next()) {
+	                b = buildObject(rs);
+	            }
+	            return b;
+	        } catch (SQLException e) {
+	            throw new DataAccessException(e, "Could not retrieve data");
+	        }
+	        }
+	    private Batch buildObject(ResultSet rs) throws DataAccessException {
+	   try {
+	           Batch b = null;
+	            if(rs.getString(columnIndex).equals) {
+	        }
+	        return null;
+
+	       }
 	
 	@Override
 	public boolean checkIfNotificationExist() throws DataAccessException {
-		
+		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean checkBatchDate() throws DataAccessException {
-		
+		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public Status getNotificationState() throws DataAccessException {
-		
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Batch getBatch() throws DataAccessException {
-		
+		// TODO Auto-generated method stub
 		return null;
 	}
 
