9b7da000d307dbf08a1b4a465e77902e242bb495
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-11 11:00:35 +0200

    Updated Batch DBs

--- DPR/src/db/BatchDBIF.java
+++ DPR/src/db/BatchDBIF.java
@@ -1,8 +1,12 @@
 package db;
 
+import java.util.List;
+
 import model.Batch;
+import model.Status;
 
 public interface BatchDBIF {
 	public Batch searchBatch(int batchID) throws DataAccessException;
+	public List<Batch> findAllByStatus(Status status) throws DataAccessException;
 
 }
