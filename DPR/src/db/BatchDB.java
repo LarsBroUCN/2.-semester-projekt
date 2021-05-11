b42a29efaf5760505734b3bd5ed75c58b1219270
perwolf <1086271@ucn.dk>
2021-05-11 08:44:14 +0200

    Begyndt på BatchDB. Ikke færdig.

--- DPR/src/db/BatchDB.java
+++ DPR/src/db/BatchDB.java
@@ -1,31 +1,69 @@
 package db;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import model.Batch;
+import model.Product;
 import model.Status;
 
 public class BatchDB implements BatchDBIF {
-
+	
+	private final String SEARCH_BATCH_Q = "select * from batch where BatchID = ?";
+	private PreparedStatement searchBatch;
+	
+	public BatchDB() throws DataAccessException {
+		try {
+			searchBatch = db.DBConnection.getInstance().prepareStatement(SEARCH_BATCH_Q);
+		} catch(SQLException e) {
+			throw new DataAccessException(e, "Could not prepare statement");
+		}
+	}
+	
+	public Batch searchBatch(int batchID) throws DataAccessException {
+		try {
+			searchBatch.setInt(1, batchID);
+			ResultSet rs = searchBatch.executeQuery();
+			Batch b = null;
+			if(rs.next()) {
+				b = buildObject(rs);
+			}
+			return b;
+		} catch (SQLException e) {
+			throw new DataAccessException(e, "Could not retrieve data");
+		}
+		}
+	private Batch buildObject(ResultSet rs) throws DataAccessException {
+		try {
+			Batch b = null;
+			if(rs.getString(columnIndex).equals)
+		}
+		return null;
+	}
+	
+	
 	@Override
 	public boolean checkIfNotificationExist() throws DataAccessException {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public boolean checkBatchDate() throws DataAccessException {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public Status getNotificationState() throws DataAccessException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Batch getBatch() throws DataAccessException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
