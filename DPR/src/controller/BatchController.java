7baca96b8e4748390182ff2ec1f6faa3e16f58a6
LeonNorsk <76209144+LeonNorsk@users.noreply.github.com>
2021-05-11 09:27:09 +0200

    med pseudo kode

--- DPR/src/controller/BatchController.java
+++ DPR/src/controller/BatchController.java
@@ -8,11 +8,16 @@ public class BatchController {
 
 	public static  ArrayList<Batch> generateExpiredList() {
 		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	public static ArrayList<Batch> generatePendingList() {
 		// TODO Auto-generated method stub
+		//check batch om den har en notifikation --> og hvis den ikke har en 
+		//check datoen på batch
+		//hvis dato har nået warningperiod oprettes en notifikation med status pending
+		//hent batches med status pending
 		return null;
 	}
 
