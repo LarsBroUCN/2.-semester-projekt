44995a2ec4b8900a75490adffab416b910994ca8
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-11 10:37:55 +0200

    Commented out redundant code

--- DPR/src/db/BatchDB.java
+++ DPR/src/db/BatchDB.java
@@ -46,28 +46,28 @@ public class BatchDB implements BatchDBIF {
 
 	       }
 	
-	@Override
-	public boolean checkIfNotificationExist() throws DataAccessException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean checkBatchDate() throws DataAccessException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Status getNotificationState() throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Batch getBatch() throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	@Override
+//	public boolean checkIfNotificationExist() throws DataAccessException {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean checkBatchDate() throws DataAccessException {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+//
+//	@Override
+//	public Status getNotificationState() throws DataAccessException {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public Batch getBatch() throws DataAccessException {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
