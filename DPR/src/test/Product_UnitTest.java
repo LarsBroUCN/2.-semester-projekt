77294fbfc94653ea14468223492ed46477b29b7a
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-19 13:25:37 +0200

    Update Product_UnitTest.java

--- DPR/src/test/Product_UnitTest.java
+++ DPR/src/test/Product_UnitTest.java
@@ -29,19 +29,6 @@ public class Product_UnitTest {
 		p.setProductName(newName);
 		assertEquals(newName, p.getProductName());
 	}
-	
-	@Test
-	public void getBarcodeTest() {
-		String s = p.getBarcode();
-		assertEquals("123456789", s);
-	}
-	
-	@Test
-	public void setBarcodeTest() {
-		String newBarcode = "Some new text";
-		p.setBarcode(newBarcode);
-		assertEquals(newBarcode, p.getBarcode());
-	}
 		
 	@Test
 	public void getPriceTest() {
