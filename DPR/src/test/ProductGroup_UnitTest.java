2d7a1e5801716c4acc0bbfd9bd6db6ebdcd00afc
Kviksand <72926418+Kviksand@users.noreply.github.com>
2021-05-19 11:41:20 +0200

    Update ProductGroup_UnitTest.java

--- DPR/src/test/ProductGroup_UnitTest.java
+++ DPR/src/test/ProductGroup_UnitTest.java
@@ -10,38 +10,42 @@ import model.ProductGroup;
 public class ProductGroup_UnitTest {
 
 	ProductGroup pg;
-	
+
 	@Before
 	public void setUp() {
 		pg = new ProductGroup("some name", 10);
 	}
-	
+
+	// Might remove
+	@Test
+	public void objectExistTest() {
+		assertNotNull(pg);
+	}
+
 	@Test
 	public void getProductGroupNameTest() {
 		String name = pg.getGroupName();
 		assertEquals("some name", name);
 	}
-	
+
 	@Test
 	public void setProductGroupNameTest() {
 		String name = "new name";
 		pg.setGroupName(name);
 		assertEquals(name, pg.getGroupName());
 	}
-	
+
 	@Test
 	public void getGroupWarningPeriod() {
 		int i = pg.getGroupWarningPeriod();
 		assertEquals(10, i);
 	}
-	
+
 	@Test
 	public void setGroupWarningPeriod() {
 		int newInt = 1;
 		pg.setGroupWarningPeriod(newInt);
 		assertEquals(newInt, pg.getGroupWarningPeriod());
 	}
-	
-	
 
 }
