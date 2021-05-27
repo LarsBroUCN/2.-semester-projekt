290013d229f478d678f3f84e4504f116ac987ea7
LeonNorsk <76209144+LeonNorsk@users.noreply.github.com>
2021-05-27 14:05:22 +0200

    rodet op i scripts

--- DPR/res/Insert_To_Tables.sql
+++ DPR/res/Insert_To_Tables.sql
@@ -23,48 +23,21 @@ use dmab0920_1086231
 --insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (66666, 'Dame', 12, 99, 'Tekstil & sko')
 --insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (77777, 'Hus og Have', 12, 300, 'Bolig & fritid')
 --insert into products (barcode, productName, productWarningperiod, price, groupname_fk ) values (88888, 'Ost', 12, 59, 'Brød, bageri & mejeri')
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/14','2021/06/01',  12, 11111) --burde ikke lave en notifikation                            -- virker korrekt
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/14','2021/05/20',  12, 22222)  --burde lave en notifikation og sætte status til pending    -- virker korrekt
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/14','2021/07/01',  12, 11111) --burde ikke lave en notifikation                            -- virker korrekt
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/14','2021/06/15',  12, 22222)  --burde lave en notifikation og sætte status til pending    -- virker korrekt
 --insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/12',  12, 44444)	--burde sætte sig selv til expired                          -- virker korrekt
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/20',  12, 66666)	--sættes til discount 
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/24',  12, 77777)	--sættes til complete 
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/14',  12, 33333)	--sættes til discard 
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/06/01',  12, 66666)	--sættes til discount 
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/26',  12, 77777)	--sættes til complete 
+--insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/26',  12, 33333)	--sættes til discard 
 
 
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCOUNT', 3)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'COMPLETE', 4)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCARD', 5)
-
-
-
-
-
-
-
-
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCOUNT', 4)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'COMPLETE', 5)
+--insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCARD', 3)
 
 
 --yyyy-mm-dd
---Ikke i brug
 
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/06/01',  12, 44444)  --burde ikke lave en notifikation
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/24',  12, 55555)	--burde lave en notifikation og sætte status til pending
---insert into batches (arrivaldate, expirationdate, warningperiod, barcode_fk ) values ('2021/05/13','2021/05/24',  12, 88888)	--sættes  til pending
-
-
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCOUNT', 3)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'COMPLETE', 4)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCARD', 5)
---insert into notifications (discount, note, status, batchID_fk) values (25, '', 'PENDING', 8)
-
---insert into notifications (discount, note, status, batchID_fk) values (25, '', 'PENDING', 8)
---insert into notifications (discount, note, status, batchID_fk) values (10, '', 'PENDING', 1)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'PENDING', 3)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'PENDING', 5)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCOUNT', 5)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'COMPLETE', 6)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'DISCARD', 7)
---insert into notifications (discount, note, status, batchID_fk) values (5, '', 'EXPIRED', 4)
 
 
 
--- DPR/res/select&forskelligejoins.sql
+++ DPR/res/select&forskelligejoins.sql
@@ -13,50 +13,5 @@ use dmab0920_1086231
 
 
 
-  -- update notifications set discount = 30, note = '' , status = 'expired'  where batchid_fk = 5
-
-
- -- select notifications.batchid_fk, notifications.status from notifications
- --inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'discount'
-
- --  select notifications.batchid_fk, notifications.status from notifications
- --inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'pending'
-
- -- select notifications.batchid_fk, notifications.status from notifications
- --inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'complete'
-
- -- select notifications.batchid_fk, notifications.status from notifications
- --inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'discard'
-
- --  select notifications.batchid_fk, notifications.status from notifications
- --inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'expired'
-
--- select batches.batchID where not.ba
- --select * from batches where batches.batchID  in (select * from notifications where Notifications.batchID_fk =batches.batchID ) 
---select notifications.batchid_fk, notifications.status, batches.batchID, batches.arrivaldate, 
---				batches.warningperiod, batches.expirationdate, batches.barcode_fk from notifications
---				inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'PENDING'
-
---select notifications.batchid_fk, notifications.status, batches.batchID, batches.arrivaldate, 
---			 batches.warningperiod, batches.expirationdate, batches.barcode_fk from notifications
---			 inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'EXPIRED'
-
---select Products.barcode, Products.productName, Products.productWarningperiod, Products.price, Products.groupname_fk from Products
---inner join batches on batches.barcode_fk = Products.barcode
-
---select * from batches where barcode_fk = 122121
-
---update batches set arrivaldate =?, expirationdate=?, warningperiod=?, barcode_fk=? where batchID=4
-
-
- --select * from Products where Products.barcode in (select * from batches where Batches.barcode_fk) 
-
- --finder de batches som ikke er knyttet til notifikationer--
- --select * from batches where batches.batchID not in (select Notifications.batchID_fk from notifications)
-
-
-
-
- --select notifications.batchid_fk, notifications.status, batches.batchID, batches.arrivaldate,  batches.warningperiod, batches.expirationdate, batches.barcode_fk from notifications
-	--		inner join Batches on batches.batchid = Notifications.batchID_fk where notifications.status = 'PENDING'
+ 
 
